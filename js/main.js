6870f286a28df31138589755798e52adf39fd212
gitprzemek <przemyslaw.sz.home@gmail.com>
2018-04-24 19:15:24 +0200

    add better responsive

--- js/main.js
+++ js/main.js
@@ -9,16 +9,16 @@ function setDate() {
 
     const seconds = nowTime.getSeconds();
     const secondsDeg = (seconds / 60) * 360;
-    secondHand.style.transform = `translate(-50%) scale(0.98) rotate(${secondsDeg}deg)`;
+    secondHand.style.transform = `translate(-50%, -50%) scale(0.98) rotate(${secondsDeg}deg)`;
 
 
     const minutes = nowTime.getMinutes();
     const minutesDeg = (minutes / 60) * 360;
-    minuteHand.style.transform = `translate(-50%) scale(0.92) rotate(${minutesDeg}deg)`;
+    minuteHand.style.transform = `translate(-50%, -50%) scale(0.92) rotate(${minutesDeg}deg)`;
 
     const hour = nowTime.getHours();
     const hourDeg = (hour / 12) * 360;
-    hourHand.style.transform = `translate(-50%) scale(0.8) rotate(${hourDeg}deg)`;
+    hourHand.style.transform = `translate(-50%, -50%) scale(0.8) rotate(${hourDeg}deg)`;
 
     if(secondsDeg === 0){
         allHands.forEach(hand => hand.style.transition = "none")
